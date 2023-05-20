ac4e43afe1b8f074fb695a149dcc430a8dd66727
jin mo byeon <107828109+byeonjinmo@users.noreply.github.com>
2023-05-20 21:45:24 +0900

    Add files via upload
    실제 로그인 기능 구현 부분에서 로그인이 되었을 때 1값을, 실패시 -1 값을 int 형으로 반환하기 위해 login 메서드를 void에서 int로 변경하였고 결과적으로 로그인 시 상태를 확인할 수 있도록 수정

---
+++ LockerLibrary/src/member/manageMember.java
@@ -0,0 +1,156 @@
+/*
+ * @ 해결과제 :
+ * 
+ * 1. allLockers, occupiedLockers를 따로 분리해서 저장 -> (일단 싱글톤 객체, 나중되면 서버에 저장)
+ * 2. locker를 main에서 일일히 만들지 말고, 미리 만들어둔 KBU 사물함 목록이 있으면 좋겠다.
+ * 
+*/
+package member;
+
+import member.model.Member;
+import locker.model.Locker;
+import locker.LockerList;
+
+import java.util.Map;
+import java.util.HashMap;
+import java.util.List;
+import java.util.ArrayList;
+
+public class manageMember<T> {
+    private Member member;
+
+    private boolean loggedIn;
+    private int loginAttempts;
+    private Map<String, String> userCredentials; // 사용자 계정 정보 저장
+
+    private static List<Locker> allLockers = LockerList.getAllLockers(); // 전체 locker 목록 (싱글톤 객체 생성)
+    private static HashMap<Integer, Locker> occupiedLockers = LockerList.getOccupiedLockers(); // 예약된 locker 목록 (싱글톤 객체 생성)
+
+    public manageMember() {
+        userCredentials = new HashMap<>();
+    }
+
+    public void setMember(Member object) {
+        this.member = object;
+        System.out.println(member.getter());
+    }
+
+
+    /*로그인 기능 구현*/
+    // 회원가입
+    public void signUp(String username, String password) {
+        if (loggedIn) {
+            System.out.println("로그인 상태에서는 회원가입이 불가능합니다.");
+            return;
+        }
+        if (userCredentials.containsKey(username)) {
+            System.out.println("이미 존재하는 사용자명입니다.");
+            return;
+        }
+
+        userCredentials.put(username, password);
+        System.out.println("회원가입이 완료되었습니다.");
+    }
+
+    // 로그인
+    public int login(String username, String password) {
+        // 추가적인 로그인 조건을 확인
+        if (loggedIn) {
+            System.out.println("이미 로그인되어 있습니다. 로그아웃을 실행한 후에 다시 로그인 시도해주세요.");
+            return -1;
+        }
+        // 실제 로그인 기능 구현
+        if (userCredentials.containsKey(username) && userCredentials.get(username).equals(password)) {
+            loggedIn = true;
+            loginAttempts = 0;
+            System.out.println("로그인 성공!");
+            return 1;
+        } else {
+            loggedIn = false;
+            loginAttempts++;
+            System.out.println("로그인 실패!");
+            return -1;
+        }
+    }
+
+    // 로그아웃
+    public void logout() {
+        loggedIn = false;
+        System.out.println("로그아웃 되었습니다.");
+    }
+
+    // 로그인 상태 체크
+    public boolean login_check() {
+        return loggedIn;
+    }
+
+
+    /* 예약 기능 구현 */
+    // 빈 사물함 목록 출력
+    public List<Locker> check_unoccupiedLocker() {
+        List<Locker> unoccupied = new ArrayList<>();
+
+        if(occupiedLockers.size() == 0) { // 예약된 locker가 없으면, 전체 사물함 리턴
+            return allLockers;
+        }
+        
+        for(Locker locker : allLockers) { // 전체 locker를 순회하며, (예약된 locker제외) unoccupied 목록에 추가
+            if(!occupiedLockers.containsValue(locker))
+                unoccupied.add(locker);
+        } 
+        return unoccupied;
+    }
+
+    // 예약 : 예약된 사물함 목록에 [키=pk, 값=locker] 저장  (pk에 학번 넣는게 바람직)
+    public void reserve_locker(int pk, Locker locker) {
+        if(!loggedIn) {
+            System.out.println("로그인이 필요합니다.");
+            return;
+        }
+        if(occupiedLockers.containsKey(pk)) {
+            System.out.println("이미 다른 사물함을 예약중입니다.");
+            return;
+        }
+        if(occupiedLockers.containsValue(locker)) {
+            System.out.println("예약된 사물함입니다.");
+            return;
+        }
+        
+        occupiedLockers.put(pk, locker); // Hashmap에 [pk, locker] 저장
+        System.out.printf("%s-%d 사물함이 예약되었습니다.\n", locker.getbuildingName(), locker.getLockNum());
+    }
+
+    // 예약취소 : 예약된 사물함 목록에서 키=pk에 해당하는 요소 제거
+    public void cancel_locker(int pk) {
+        if(!loggedIn) {
+            System.out.println("로그인이 필요합니다.");
+            return;
+        }
+        if(!occupiedLockers.containsKey(pk)) {
+            System.out.println("예약된 사물함이 없습니다.");
+            return;
+        }
+        
+        occupiedLockers.remove(pk);
+        System.out.println("사물함이 예약취소 되었습니다.");
+    }
+
+    // 예약확인 : 사용중인 사물함 목록에서 키=pk에 해당하는 Locker객체 반환 +정보 출력  (void로 출력만 해도??)
+    public Locker check_locker(int pk) {
+        Locker myLocker = occupiedLockers.get(pk);
+        
+        if(!loggedIn) {
+            System.out.println("로그인이 필요합니다.");
+            return null;
+        }
+        if(myLocker == null) {
+            System.out.println("예약된 사물함이 없습니다.");
+            return null;
+        }
+
+        System.out.printf("%s-%d 사물함을 예약중입니다.\n", myLocker.getbuildingName(), myLocker.getLockNum());
+        return myLocker;
+    }
+}
+
+
---
+++ LockerLibrary/src/member/model/Member.java
@@ -0,0 +1,34 @@
+package member.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * <h3>회원정보 관리 추상클래스</h3>
+ * <p>필드에 필수 변수 3개를 제공하며, 제약 없이 변수를 넣을 수 있다.</p>
+ * <p>해당 클래스를 상속받아 변수의 이름을 재정의함으로써, 유지보수를 쉽게 할 수 있다는 장점이 있다.</p>
+ * <p>사용시 유의점, Member 클래스를 그대로 사용할 경우 변수 순서를 사용자가 관리하여야 한다. (유지보수가 어렵다.)
+ * 그 때문에, 클래스를 상속받아서 변수를 재정의하는 것이 좋다.</p>
+ * <p>ex) 변수 이름: name, gender, phoneNumber</p>
+ * @param <T>
+ */
+public abstract class Member<T> {
+    private T member1;
+    private T member2;
+    private T member3;
+
+    public Member(T member1, T member2, T member3) {
+        this.member1 = member1;
+        this.member2 = member2;
+        this.member3 = member3;
+    }
+
+    public List getter() {
+        List list = new ArrayList<>();
+        list.add(member1);
+        list.add(member2);
+        list.add(member3);
+        return list;
+    }
+
+}
